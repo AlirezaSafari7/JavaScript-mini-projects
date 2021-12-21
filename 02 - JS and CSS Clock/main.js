34f80072f4a1bae20e0cf6846d0d8eba701c2020
Alireza <alirezasa7700@gmail.com>
2021-12-21 18:44:19 +0330

    add second, minute and hour to main.js

--- 02 - JS and CSS Clock/main.js
+++ 02 - JS and CSS Clock/main.js
@@ -1,5 +1,18 @@
 "use strict"
+const secondHand = document.querySelector('.second-hand');
+const minHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+
 function setDate() {
-    console.log('Hi');
+    const now = new Date();
+    const seconds = now.getSeconds();
+    const minute = now.getMinutes();
+    const hour = now.getHours();
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    const minutesDegrees = ((minute / 60) * 360) + 90;
+    const hoursDegrees = ((hour / 60) * 360) + 90;
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+    minHand.style.transform = `rotate(${minutesDegrees}deg)`;
+    hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
 }
 setInterval(setDate, 1000);
